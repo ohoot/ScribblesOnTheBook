e0da45c5e91e643e7fdd899c2ace1cb3cb26b21b
ohoot <ohoot@naver.com>
2016-02-23 16:20:07 +0900

    .

--- app/src/main/java/com/example/joo/scribblesonthebook/FindPassword1Activity.java
+++ app/src/main/java/com/example/joo/scribblesonthebook/FindPassword1Activity.java
@@ -19,11 +19,11 @@ public class FindPassword1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_password1);
-        this.setTitle("비밀번호 찾기");
+        setTitle("비밀번호 찾기");
 
         editEmail = (EditText) findViewById(R.id.edit_email_find_password);
 
-        Button btn = (Button) findViewById(R.id.btn_cancel);
+        Button btn = (Button) findViewById(R.id.btn_email_cancel);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,15 +36,8 @@ public class FindPassword1Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FindPassword1Activity.this, FindPassword2Activity.class);
-                Random r = new Random();
-                intent.putExtra(FindPassword2Activity.CONFIRM_KEY, r.nextInt(100));
                 startActivity(intent);
             }
         });
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        super.setTitle(title);
     }
 }
--- app/src/main/java/com/example/joo/scribblesonthebook/FindPassword2Activity.java
+++ app/src/main/java/com/example/joo/scribblesonthebook/FindPassword2Activity.java
@@ -3,23 +3,36 @@ package com.example.joo.scribblesonthebook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.joo.scribblesonthebook.R;
 
 public class FindPassword2Activity extends AppCompatActivity {
     public static final String CONFIRM_KEY = "confirm";
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_password2);
+        setTitle("비밀번호 찾기");
 
-        textView = (TextView) findViewById(R.id.testText);
-        Intent intent = getIntent();
-        int i = intent.getIntExtra(CONFIRM_KEY, 0);
+        Button btn = (Button) findViewById(R.id.btn_confrimkey_cancel);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-        textView.setText(i + "");
+        btn = (Button) findViewById(R.id.btn_change_password);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindPassword2Activity.this, FindPassword3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
