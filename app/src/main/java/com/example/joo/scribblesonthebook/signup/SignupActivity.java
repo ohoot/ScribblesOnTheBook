dcc3d67ff499b7899d26d20cc830c77cda67bd3b
ohoot <ohoot@naver.com>
2016-03-16 21:29:39 +0900

    .

--- app/src/main/java/com/example/joo/scribblesonthebook/signup/SignupActivity.java
+++ app/src/main/java/com/example/joo/scribblesonthebook/signup/SignupActivity.java
@@ -2,18 +2,26 @@ package com.example.joo.scribblesonthebook.signup;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.joo.scribblesonthebook.R;
 
 public class SignupActivity extends AppCompatActivity {
     public static final String REGISTER_USER_BACKSTACK = "registerUserInfo";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(R.layout.view_actionbar_signup);
+
 
         Fragment fragment = new SignupUserFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
